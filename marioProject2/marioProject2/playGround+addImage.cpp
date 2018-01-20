1c3950adfa5f1a3195ccf9f28dbf64470ec354a8
DESKTOP-D7ID7GB\HAN <aka8880@gmail.com>
2018-01-20 19:35:50 +0900

    [이미지] playGround + addImage 이미지 조금 추가

--- marioProject2/marioProject2/playGround+addImage.cpp
+++ marioProject2/marioProject2/playGround+addImage.cpp
@@ -3,9 +3,8 @@
 
 void playGround::addImage()
 {
-	
-
-
+	IMAGEMANAGER->addFrameImage("베이지마리오대기_우", "./image/mario_basic_idle_right.bmp", 70, 135, 1, 1, true, RGB(255, 0, 255));IMAGEMANAGER->addFrameImage("베이지마리오대기_우", "./image/mario_basic_idle_right.bmp", 70, 135, 1, 1, true, RGB(255, 0, 255));
+	IMAGEMANAGER->addFrameImage("베이지마리오대기_좌", "./image/mario_basic_idle_left.bmp", 70, 135, 1, 1, true, RGB(255, 0, 255));
 
 
 
