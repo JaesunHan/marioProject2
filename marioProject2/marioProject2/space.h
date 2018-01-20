bb2f7b039187f4622e2802953a8c062e32698f48
JaesunHan <kapoochino93@gmail.com>
2018-01-20 11:22:16 +0900

    add all files

---
+++ marioProject2/marioProject2/space.h
@@ -0,0 +1,42 @@
+#pragma once
+#include "gameNode.h"
+#include "ship.h"
+
+//화면은 9등분으로 나눈다
+enum DIRECTION
+{
+	DIRECTION_LEFT_TOP,
+	DIRECTION_MIDDLE_TOP,
+	DIRECTION_RIGHT_TOP,
+	DIRECTION_LEFT_MIDDLE,
+	DIRECTION_MIDDLE_MIDDLE,
+	DIRECTION_RIGHT_MIDDLE,
+	DIRECTION_LEFT_BOTTOM,
+	DIRECTION_MIDDLE_BOTTOM,
+	DIRECTION_RIGHT_BOTTOM
+};
+
+class space : public gameNode
+{
+private:
+	image* _background;
+	int _mapSizeWidth;
+	int _mapSizeHeight;
+
+	ship* _ship;
+
+public:
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	DIRECTION getDirection(int x, int y);
+
+	void setShip(ship* obj) { _ship = obj; }
+
+
+	space();
+	~space();
+};
+
---
+++ marioProject2/marioProject2/titleScene.h
@@ -0,0 +1,25 @@
+#pragma once
+#include "gameNode.h"
+class titleScene : public gameNode
+{
+private : 
+	image* _image[3];
+	
+	int _x[3];
+	int _y[3];
+
+
+	int _selectX;
+	int _selectY;
+
+public:
+
+	HRESULT init();
+	void release();
+	void update();
+	void render();
+
+	titleScene(void);
+	~titleScene(void);
+};
+
