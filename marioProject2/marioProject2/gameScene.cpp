bc363343bafcb37e04048d662636705bfb6b7442
samusaran <neitt@naver.com>
2018-01-20 18:11:45 +0900

    커밋

--- marioProject2/marioProject2/gameScene.cpp
+++ marioProject2/marioProject2/gameScene.cpp
@@ -17,6 +17,8 @@ HRESULT gameScene::init()
 {
 	IMAGEMANAGER->addImage("°ÔÀÓ¾À¹é±×¶ó¿îµå", ".\\image\\playSceneBackgroundFront.bmp", 960, 720, true, RGB(255, 0, 255));
 	IMAGEMANAGER->addImage("°ÔÀÓ¾À·¹ÀÌ¾Æ¿ô", ".\\image\\playSceneBackgroundBack.bmp", 960, 720, true, RGB(255, 0, 255));
+	_EM = new enemyManager;
+	_EM->init();
 
 	return S_OK;
 }
@@ -28,11 +30,12 @@ void gameScene::release()
 
 void gameScene::update() 
 {
-
+	_EM->update();
 }
 
 void gameScene::render() 
 {
 	IMAGEMANAGER->findImage("°ÔÀÓ¾À·¹ÀÌ¾Æ¿ô")->render(getMemDC(), 0, 0);
 	IMAGEMANAGER->findImage("°ÔÀÓ¾À¹é±×¶ó¿îµå")->render(getMemDC(), 0, 0);
+	_EM->render();
 }
--- marioProject2/marioProject2/gameScene.h
+++ marioProject2/marioProject2/gameScene.h
@@ -1,10 +1,12 @@
 #pragma once
 #include "gameNode.h"
 #include "player.h"
+#include "enemyManager.h"
 class gameScene : public gameNode
 {
 private:
 	player * _player;
+	enemyManager* _EM;
 	
 public:
 	gameScene();
